1cf29fb84f8de5a089356c54b25e29daa24216a7
SwapnilSalunke1625 <salunkeswapnil145@gmail.com>
2025-12-11 23:07:40 +0530

    added everthing

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,17 +1,38 @@
-import express from "express"
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
-import { app } from "./app.js"
-dotenv.config()
+// import express from "express"
+// import dotenv from "dotenv"
+// import db from "./db/index.js"
 
-const PORT=process.env.PORT || 1010
+// import { app } from "./app.js"
+// dotenv.config()
 
-connectDB()
-.then(()=>{
-    app.listen(PORT, ()=>{
-        console.log(`Server is running on port : ${PORT}`)
-    })
-})
-.catch((error)=>{
-    console.log("MONGODB CONNECTION FAILED !!!", error)
-})
+// const PORT=process.env.PORT || 1010
+
+// db()
+// .then(()=>{
+//     app.listen(PORT, ()=>{
+//         console.log(`Server is running on port : ${PORT}`)
+//     })
+// })
+// .catch((error)=>{
+//     console.log("MONGODB CONNECTION FAILED !!!", error)
+// })
+
+
+
+// SQL connection is setup
+
+import express from "express";
+import dotenv from "dotenv";
+import db from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 1010;
+
+// MySQL already connects automatically using createPool
+// So just start server
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port: ${PORT}`);
+});
